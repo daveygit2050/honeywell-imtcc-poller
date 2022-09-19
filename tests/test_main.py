78c3cdff716ea91e6ae1e95308777078436dc414
Dave Randall <19395688+daveygit2050@users.noreply.github.com>
2022-09-19 12:15:15 +0100

    Add acceptance test boilerplate

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,10 @@
-def test_main():
-    assert True
+import sys
+import pytest
+
+import honeywell_imtcc_poller
+
+def test_main(mocker):
+    mocker.patch.object(sys, "argv", ["poll-honeywell", "--help"])
+    with pytest.raises(SystemExit) as system_exit:
+        honeywell_imtcc_poller.run_cli()
+    assert system_exit.value.code == 0
